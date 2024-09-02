307e8390022b5ead9779a6179eafdb99ddef7558
Jan <jan.peterhaensel@adconova.com>
2024-09-02 19:33:11 +0200

    aaaaa

--- main.py
+++ main.py
@@ -12,23 +12,35 @@ class matrix():
     self.rows = []
     for i in range(0, len(pins)):
       self.rows.append(NeoPixel(pins[i],columns))
-    self.letter_count = 0
-    self.letter_width = 3
+    self.columns = columns
+    self.letter_index = 1
     self.letters = {}
-    self.letters["i"] = [[0,0,0],[0,1,0],[0,1,0],[0,1,0]]
-
-  def write_letter(self,letter):
+    self.letters["i"] = [[1],[1],[1],[1]]
+    self.letters["a"] = [[1,1,1,],[1,0,1,],[1,1,1,],[1,0,1,]]
+  def write_letter(self,letter: str):
     for idi, i in enumerate(self.letters[letter]):
       for idj, j in enumerate(i):
-        print(idi)
-        self.rows[idi][idj + (self.letter_width * self.letter_count)] = (j*255,0,0)
+        self.rows[idi][idj + (self.letter_index)] = (j*255,0,0)
+      self.rows[idi].write()
+
+    self.letter_index += len(self.letters[letter][0]) + 1
+
+  def write_string(self,s: str):
+    for letter in s:
+      self.write_letter(letter)
+
+  def clear(self):
     for i in range(0,len(self.rows)):
+      for j in range(0,self.columns):
+        self.rows[i][j] = (0,0,0)
       self.rows[i].write()
-    self.letter_count +=1
+    self.letter_index = 1
 
 def main():
   m = matrix([Pin(13),Pin(12),Pin(14),Pin(27)],10)
-  m.write_letter("i")
-  m.write_letter("i")
+  m.write_string("ia")
+  sleep(1)
+  m.clear()
+  m.write_string("ai")
 if __name__ == "__main__":
   main()
